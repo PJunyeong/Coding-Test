516f683458088214715557ab34763f7f7f30412e
j_aion <95460398+PJunyeong@users.noreply.github.com>
2022-09-24 23:11:47 +0900

    0924

---
+++ LV3/호텔 방 배정.py
@@ -0,0 +1,17 @@
+import sys
+
+def solution(k, room_number):
+    sys.setrecursionlimit(10000000)
+    rooms = {}
+
+    def find(node):
+        if node not in rooms:
+            rooms[node] = node + 1
+            return node
+        else:
+            room = find(rooms[node])
+            rooms[node] = room + 1
+            return room
+
+    results = [find(number) for number in room_number]
+    return results
